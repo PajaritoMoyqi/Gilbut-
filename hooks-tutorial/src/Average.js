d278b21fd7c5027f006c1a8c8e33830fc0b9a6c9
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-19 20:52:36 +0900

    useCallback done

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 
 const getAverage = numbers => {
   console.log('calculating...');
@@ -12,13 +12,13 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
-  }
-  const onInsert = e => {
+  }, []);
+  const onInsert = useCallback(() => {
     setList([...list, parseInt(number)]);
     setNumber('');
-  }
+  }, [list, number]);
 
   const avg = useMemo(() => getAverage(list), [list])
 
