f36c9e419d9601b6361a440da4433c2e5cd8b23e
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-21 11:10:04 +0900

    newslist done

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import axios from 'axios';
+import NewsList from './NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?language=es&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(`https://newsapi.org/v2/top-headlines?language=es&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // return (
+  //   <div>
+  //     <div><button onClick={onClick}>GET NEWS</button></div>
+  //     { data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly /> }
+  //   </div>
+  // );
 
   return (
-    <div>
-      <div><button onClick={onClick}>GET NEWS</button></div>
-      { data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly /> }
-    </div>
-  );
+    <NewsList />
+  )
 }
 
 export default App;
