c39441b23c5e4d28f45595e3e15624974e7057f3
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-21 10:22:07 +0900

    notfound page added

---
+++ router-tutorial/src/pages/NotFound.js
@@ -0,0 +1,19 @@
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 64,
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      404 NOT FOUND
+    </div>
+  )
+}
+
+export default NotFound;
