c8b201cd6b1c9301dd5c86f4ddcf84e7fec5b2a2
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-21 18:03:31 +0900

    react-redux - container component done

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,17 @@
+import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+}
+
+const mapStateToProps = state => ({
+  number: state.counter.number
+});
+const mapDispatchToProps = {
+  increase, decrease
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
