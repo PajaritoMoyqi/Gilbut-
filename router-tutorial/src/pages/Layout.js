16fe9729cb707f06ec4e26eddfc26ab5218bec41
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-21 10:10:35 +0900

    useNavigate

--- router-tutorial/src/pages/Layout.js
+++ router-tutorial/src/pages/Layout.js
@@ -1,10 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  }
+  const goArticles = () => {
+    navigate('/articles', { replace: true });
+  }
+
   return (
     <div>
       <header style={{ background: 'lightgray', padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>go back</button>
+        <button onClick={goArticles}>go Articles List</button>
       </header>
       <main>
         <Outlet />
