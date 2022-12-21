5bf1d53c305d419a3d838dc0a4e7f705725ba3a4
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-21 12:13:25 +0900

    react router applied

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,16 +1,12 @@
-import { useState, useCallback } from 'react';
-import NewsList from './NewsList';
-import Categories from './Categories';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPages';
 
-function App() {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
+const App = () => {
   return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   )
 }
 
