acdde628eb178f4a0747742c9ecbd9c913673a69
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-26 09:00:15 +0900

    router practice for essay ongoing

---
+++ router-tutorial/src/sample/Article.js
@@ -0,0 +1,9 @@
+const Article = () => {
+  return (
+    <>
+      <h1>Article Page</h1>
+    </>
+  )
+}
+
+export default Article;
---
+++ router-tutorial/src/sample/ArticlesList.js
@@ -0,0 +1,9 @@
+const ArticlesList = () => {
+  return (
+    <>
+      <h1>Articles List Page</h1>
+    </>
+  )
+}
+
+export default ArticlesList;
